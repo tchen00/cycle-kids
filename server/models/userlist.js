c191f7bdb7d53c80780622657e7e49292fb17884
Tammy Chen <tchen00@stuy.edu>
2021-11-13 21:39:39 -0500

    restart

--- server/models/userlist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserListSchema = new mongoose.Schema({
-  logged:[String],
-});
-
-// compile model from schema
-module.exports = mongoose.model("userlist", UserListSchema);
