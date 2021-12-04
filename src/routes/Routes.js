df8b2005f05cd321dd29f183e8e914928a3df0d6
Lauren Shrack <lauren@shrack.net>
2021-12-04 16:58:07 -0500

    added separate pages for individual videos

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -4,11 +4,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import LoginPage from '../pages/LoginPage'
 import SignupPage from '../pages/SignupPage'
 import VideosPage from '../pages/VideosPage'
+import HelmetFitPage from '../pages/HelmetFitPage'
+import BikeABCsPage from '../pages/BikeABCsPage'
+import GearingAndBrakingPage from '../pages/GearingAndBrakingPage'
+import RoadAndCommunicationPage from '../pages/RoadAndCommunicationPage'
 
 const routes = () => {
 	return (
     <Routes>
       <Route path="/videos" element={<VideosPage />}/>
+      <Route path="/videos/helmet-fit" element={<HelmetFitPage />}/>
+      <Route path="/videos/bike-abcs" element={<BikeABCsPage />}/>
+      <Route path="/videos/gearing-and-braking" element={<GearingAndBrakingPage />}/>
+      <Route path="/videos/road-and-communication" element={<RoadAndCommunicationPage />}/>
       <Route path="/signup" element={<SignupPage />}/>
       <Route path="/" element={<LoginPage />}/>
     </Routes>
