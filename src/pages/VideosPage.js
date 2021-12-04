53dcfe9eb271f546f7f89befa2758dded3bbdf92
irinazoccolini <irinaz@mit.edu>
2021-12-04 16:40:12 -0500

    get user email

--- src/pages/VideosPage.js
+++ src/pages/VideosPage.js
@@ -24,7 +24,7 @@ const VideosPage = () => {
   
   const [played, setPlayed] = useState(0);
   function handlePause(){
-    console.log(user.email, played)
+    console.log(auth.currentUser.email, played)
   }
 
   // display based on user login 
