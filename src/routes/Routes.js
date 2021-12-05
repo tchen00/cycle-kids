e310d61f3e789457ff74dcf321cd54ebd99e2f94
Julia Camacho <49563129+juliacamacho@users.noreply.github.com>
2021-12-04 21:48:55 -0500

    add admin users page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,6 +8,7 @@ import HelmetFitPage from '../pages/HelmetFitPage'
 import BikeABCsPage from '../pages/BikeABCsPage'
 import GearingAndBrakingPage from '../pages/GearingAndBrakingPage'
 import RoadAndCommunicationPage from '../pages/RoadAndCommunicationPage'
+import UsersPage from '../pages/UsersPage'
 import PasswordResetPage from "../pages/PasswordResetPage";
 
 const routes = () => {
@@ -20,6 +21,7 @@ const routes = () => {
       <Route path="/videos/road-and-communication" element={<RoadAndCommunicationPage />}/>
       <Route path="/signup" element={<SignupPage />}/>
       <Route path="/reset-password" element={<PasswordResetPage />}/>
+      <Route path="/users" element={<UsersPage />} />
       <Route path="/" element={<LoginPage />}/>
     </Routes>
   )
